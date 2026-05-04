54daa9c0b08335cb18a162c4db446ebc37428cd2
Тимур Минажетдинов <timmin2303@mail.ru>
2026-05-04 15:58:46 +0300

    core(gameplay): added move cells and frag'n'drop draft

--- client/HexaSort/Assets/Scripts/Controller/GameController.cs
+++ client/HexaSort/Assets/Scripts/Controller/GameController.cs
@@ -2,54 +2,87 @@ using Core;
 using prefabs;
 using Progress;
 using UnityEngine;
+using Utilities;
 
 namespace Controller
 {
+    internal enum GameState
+    {
+        ANIMATING,
+        SELECT,
+        MOVE
+    }
+
     public class GameController : MonoBehaviour
     {
         private const int _movesCount = 3;
 
         public GridView gridView;
+
         private readonly Cell[] _moves = new Cell[_movesCount];
+        private GameState _gameState = GameState.SELECT;
         private GridManager _gridMgr;
-        private int? _selectedCell;
+        private int _moveIndex = -1;
 
         public void Build(LevelData data)
         {
             _gridMgr = new GridManager(data.Mask, data.StartCells);
-            gridView.CreateGrid(_gridMgr);
+            gridView.CreateGrid(_gridMgr, _movesCount);
+
+            for (int i = 0; i < _movesCount; i++)
+            {
+                _moves[i] = new Cell(Creator.CreateStack(1, 2, 3));
+            }
+
+            gridView.UpdateMoves(_moves);
         }
 
-        /*private void RefreshView()
+        private void RefreshView()
         {
-            for (int i = 0; i < _grid.Cells.Count; i++)
+            /*for (int i = 0; i < _gridMgr.; i++)
             {
                 Hex top = _grid.Cells[i].Top();
                 gridView.UpdateCell(i, top);
-            }
-        }*/
+            }*/
+        }
 
-        public void OnCellClicked(int index)
+        public void OnMoveCellClicked(int index)
         {
-            /*if (_selectedCell == null)
+            if (_gameState == GameState.SELECT)
             {
-                _selectedCell = index;
+                Debug.Log("You chosen move: " + index);
+                _moveIndex = index;
+                _gameState = GameState.MOVE;
             }
             else
             {
-                bool moved = _grid.TryMove(_selectedCell.Value, index);
+                Debug.Log("You are already in move state!");
+            }
+        }
+
+        public void OnCellClicked(int x, int y)
+        {
+            if (_gameState != GameState.MOVE)
+            {
+                Debug.Log("You need to choose move!");
+            }
+            else
+            {
+                Debug.Log("You chosen cell: {" + x + ", " + y + "}");
+                bool moved = _gridMgr.TryMove(_moves[_moveIndex], x, y);
 
                 if (moved)
                 {
-                    RefreshView();
-                    if (_grid.CheckWin())
-                    {
-                        Debug.Log("You win!");
-                    }
-                }
+                    _moves[_moveIndex].Free();
+                    _moveIndex = -1;
+                    _gameState = GameState.SELECT;
 
-                _selectedCell = null;
-            }*/
+                    Debug.Log("Move success!!!");
+                    gridView.UpdateMoves(_moves);
+                    RefreshView();
+                    // TODO: Check if win
+                }
+            }
         }
     }
 }
--- client/HexaSort/Assets/Scripts/Prefabs/GridView.cs
+++ client/HexaSort/Assets/Scripts/Prefabs/GridView.cs
@@ -6,17 +6,32 @@ using UnityEngine;
 
 namespace prefabs
 {
-    public class CellClickHandler : MonoBehaviour
+    public class MoveCellClickHandler : MonoBehaviour
     {
         private GameController _controller;
         private int _index;
 
-        private void OnMouseDown() => _controller.OnCellClicked(_index);
+        private void OnMouseDown() => _controller.OnMoveCellClicked(_index);
 
         public void Init(GameController controller, int index)
         {
             _controller = controller;
             _index = index;
+        }
+    }
+
+    public class CellClickHandler : MonoBehaviour
+    {
+        private GameController _controller;
+        private int _posX, _posY;
+
+        private void OnMouseDown() => _controller.OnCellClicked(_posX, _posY);
+
+        public void Init(GameController controller, int x, int y)
+        {
+            _controller = controller;
+            _posX = x;
+            _posY = y;
         }
     }
 
@@ -33,43 +48,92 @@ namespace prefabs
 
         public GameObject hexPrefab;
         public GameController controller;
-        private HexStackPos[,] _hexStackPoses;
-        private Stack<HexView>[,] _hexStacks;
 
-        public void CreateGrid(GridManager gridMgr)
+        private List<GameObject>[,] _cellObjects;
+        private List<GameObject>[] _moveCellObjects;
+        private GameObject[] _movePlatformObjects;
+
+        private GameObject[,] _platformObjects;
+
+        private double _upperLeftX, _upperLeftY;
+
+        public void UpdateMoves(Cell[] moves)
         {
-            _hexStacks = new Stack<HexView>[gridMgr.Height, gridMgr.Width];
+            if (_moveCellObjects == null)
+            {
+                _moveCellObjects = new List<GameObject>[moves.Length];
+                for (int i = 0; i < _moveCellObjects.Length; i++)
+                {
+                    _moveCellObjects[i] = new List<GameObject>();
+                }
+            }
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                Vector3 pos = _movePlatformObjects[i].transform.position;
+
+                int level = 1;
+                foreach (Hex hex in moves[i].Items)
+                {
+                    GameObject obj = Instantiate(hexPrefab,
+                        new Vector3(pos.x, pos.y + level * (_hexaHeight * _layerThreshold), pos.z),
+                        Quaternion.identity);
+                    level++;
+
+                    _moveCellObjects[i].Add(obj);
+                    HexView view = obj.GetComponent<HexView>();
+                    view.SetColor(hex.Type);
+                }
+            }
+        }
+
+        public void CreateGrid(GridManager gridMgr, int movesCount)
+        {
+            _platformObjects = new GameObject[gridMgr.Height, gridMgr.Width];
+            _cellObjects = new List<GameObject>[gridMgr.Height, gridMgr.Width];
             for (int y = 0; y < gridMgr.Height; y++)
                 for (int x = 0; x < gridMgr.Width; x++)
                 {
-                    _hexStacks[y, x] = new Stack<HexView>();
+                    _cellObjects[y, x] = new List<GameObject>();
                 }
 
-            _hexStackPoses = new HexStackPos[gridMgr.Height, gridMgr.Width];
+            _upperLeftY = (gridMgr.Height - 1) * _centerDistanceSin60 / 2;
+            _upperLeftX = -gridMgr.Width * _centerDistance / 2;
 
-            double upperLeftY = (gridMgr.Height - 1) * _centerDistanceSin60 / 2;
-            double upperLeftX = -gridMgr.Width * _centerDistance / 2;
+            // Poses for player moves
+            _movePlatformObjects = new GameObject[movesCount];
+            for (int i = 0; i < movesCount; i++)
+            {
+                var vec = new Vector3((float)(_upperLeftX - 2 * _centerDistance - 0.3f * i), -_hexaHeight / 2,
+                    (float)_upperLeftY - 2.3f * i);
+                GameObject move = Instantiate(hexPrefab, vec, Quaternion.identity);
+                move.AddComponent<MoveCellClickHandler>().Init(controller, i);
+                _movePlatformObjects[i] = move;
+            }
 
-            int cnt = 0;
+            // Game cells
             for (int i = 0; i < gridMgr.Height; i++)
             {
                 for (int j = 0; j < gridMgr.Width; j++)
                 {
-                    float y = (float)(upperLeftY - i * _centerDistanceSin60);
-                    float x = (float)(upperLeftX + j * _centerDistance + _centerDistance * (i % 2) / 2);
+                    float y = (float)(_upperLeftY - i * _centerDistanceSin60);
+                    float x = (float)(_upperLeftX + j * _centerDistance + _centerDistance * (i % 2) / 2);
 
                     Cell cell = gridMgr.GetCell(j, i);
 
                     if (cell == null)
                         continue;
 
+                    // Create platform
                     GameObject platform =
                         Instantiate(hexPrefab, new Vector3(x, -_hexaHeight / 2, y), Quaternion.identity);
-                    platform.AddComponent<CellClickHandler>().Init(controller, cnt++);
+                    platform.AddComponent<CellClickHandler>().Init(controller, j, i);
+                    _platformObjects[i, j] = platform;
+
                     HexView platformView = platform.GetComponent<HexView>();
                     platformView.SetColor(0);
-                    _hexStacks[i, j].Push(platformView);
 
+                    // Create hexes on platform
                     int level = 0;
                     foreach (Hex hex in cell.Items)
                     {
@@ -77,11 +141,11 @@ namespace prefabs
                             new Vector3(x, _hexaHeight * _layerThreshold / 2 +
                                            level * (_hexaHeight * _layerThreshold), y),
                             Quaternion.identity);
+                        _cellObjects[i, j].Add(obj);
+
                         level++;
                         HexView view = obj.GetComponent<HexView>();
                         view.SetColor(hex.Type);
-
-                        _hexStacks[i, j].Push(view);
                     }
                 }
             }
